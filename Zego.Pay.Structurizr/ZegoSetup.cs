f6e67bbe4728ac2c0c1c45a3d84b4c32af2dd453
skhan <skhan@gozego.com>
2021-07-01 14:19:25 -0500

    Move to setup.

---
+++ Zego.Pay.Structurizr/ZegoSetup.cs
@@ -0,0 +1,20 @@
+﻿using Structurizr;
+
+namespace Zego.Pay.Structurizr
+{
+    public static class ZegoSetup
+    {
+        public static Workspace Workspace;
+        public static Model Model;
+        public static ViewSet Views;
+        public static Styles Styles;
+        public static void Initialize()
+        {
+            Workspace = new Workspace("Zego Pay", "Systems/Services/External integrations used in Zego Pay");
+            Model = Workspace.Model;
+            Model.Enterprise = new Enterprise("Zego");
+            Views = Workspace.Views;
+            Styles = Views.Configuration.Styles;
+        }
+    }
+}
